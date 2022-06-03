fb9cd6bf853794568ac2a2fee2a20633fc3192aa
wavygoten <bossmanzach007@gmail.com>
2022-06-03 17:38:24 -0500

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+type Props = {};
+
+const MainPage = (props: Props) => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default MainPage;
